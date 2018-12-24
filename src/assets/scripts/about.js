0f8076c1d438da6855360a75d50a66d407126971
Dmitriy Olin <1988navras@gmail.com>
2018-12-24 04:01:12 +0300

    first commit

--- src/assets/scripts/about.js
+++ src/assets/scripts/about.js
@@ -1,1 +1,33 @@
 import "./modules/example";
+
+
+//Секция - яндекс карты
+
+ymaps.ready(init);
+function init() {
+    var myMap = new ymaps.Map('map', {
+        center: [55.76, 37.64],
+        zoom: 10
+    });
+    myMap.behaviors.disable(['scrollZoom']);
+    
+    var coords = [ [56.00, 37.20] ];
+
+    for (var i = 0; i<coords.length; i++) {
+        
+        var myPlacemarkWithContent = new ymaps.Placemark(coords[i], {
+            hintContent: 'Здесь проживает Sponge Bob!',
+            balloonContent: 'Квадратные штаны !',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: './assets/images/icons/map_marker.svg',
+            iconImageSize: [60, 60],
+            iconImageOffset: [-24, -24],
+            iconContentOffset: [15, 15],
+    
+        });
+    
+        myMap.geoObjects
+        .add(myPlacemarkWithContent);
+    }
+}
--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,1 +1,12 @@
-console.log("auth module loaded");
+const buttonAuth = document.querySelector('.button__auth');
+const auth = document.querySelector('.auth');
+buttonAuth.addEventListener('click', function(){
+  auth.classList.add('is-flipped');
+  buttonAuth.style.visibility = 'hidden';
+})
+document.addEventListener('click', function(e){
+    if (!e.target.closest('.wrapper-auth')&&!e.target.closest('.button__auth')){
+      auth.classList.remove('is-flipped');
+      buttonAuth.style.visibility = 'visible';
+    }
+})
